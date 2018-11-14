f0045ae6b095fe065d6387c1af703dc95a962cbe
William Venner <william@venner.io>
2018-11-14 17:14:17 +0000

    Initial commit

---
+++ src/quill-to-plaintext.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace WilliamVenner;
+
+class QuillToPlaintext {
+	public function Convert($delta) {
+		$delta_obj;
+		if (is_string($delta))
+			$delta_obj = json_decode($delta);
+		elseif (is_object($delta))
+			$delta_obj = $delta;
+		
+		if (!isset($delta_obj) || !isset($delta -> ops))
+			throw new Exception('Can\'t convert invalid Quill Delta to plaintext!');
+		
+		$plaintext = '';
+		foreach($delta_obj -> ops as $op) {
+			if (isset($op -> insert) && is_string($op -> insert)) {
+				$plaintext .= $op -> insert;
+			}
+		}
+		return $plaintext;
+	}
+}
